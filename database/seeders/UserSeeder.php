023b0326278b402c3516da4d88660cd54288f5b4
Mikel Uribarren <mikel.uribarren@hettich.com>
2025-09-15 15:52:47 +0200

    Añadido seeders de usuarios

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -8,26 +8,10 @@ class UserSeeder extends Seeder
     public function run()
     {
         User::updateOrCreate(
-            ['email' => 'admin@example.com'],
-            [
-                'name' => 'Admin',
-                'password' => bcrypt('password'),
-            ]
-        );
-
-        User::updateOrCreate(
-            ['email' => 'mikel.uribarren@hettich.com'],
-            [
-                'name' => 'Mikel Uribarren',
-                'password' => bcrypt('password'),
-            ]
-        );
-
-        User::updateOrCreate(
-            ['email' => 'enaut.arrieta@hettich.com'],
+            ['email' => 'enaut.arriaga@hettich.com'],
             [
                 'name' => 'Enaut Arriaga',
-                'password' => bcrypt('password'),
+                'password' => bcrypt('hettich2025'),
             ]
         );
 
@@ -35,7 +19,7 @@ class UserSeeder extends Seeder
             ['email' => 'iker.lopetegi@hettich.com'],
             [
                 'name' => 'Iker Lopetegi',
-                'password' => bcrypt('password'),
+                'password' => bcrypt('hettich2025'),
             ]
         );
 
@@ -43,7 +27,95 @@ class UserSeeder extends Seeder
             ['email' => 'maitane.ajuria@hettich.com'],
             [
                 'name' => 'Maitane Ajuria',
-                'password' => bcrypt('password'),
+                'password' => bcrypt('hettich2025'),
+            ]
+        );
+
+        User::updateOrCreate(
+            ['email' => 'mikel.uribarren@hettich.com'],
+            [
+                'name' => 'Mikel Uribarren',
+                'password' => bcrypt('hettich2025'),
+            ]
+        );
+
+        User::updateOrCreate(
+            ['email' => 'aitor.pizarro@hettich.com'],
+            [
+                'name' => 'Aitor Pizarro',
+                'password' => bcrypt('hettich2025'),
+            ]
+        );
+
+        User::updateOrCreate(
+            ['email' => 'ignacio.angulo@hettich.com'],
+            [
+                'name' => 'Ignacio Angulo',
+                'password' => bcrypt('hettich2025'),
+            ]
+        );
+
+        User::updateOrCreate(
+            ['email' => 'jon.lesaka@hettich.com'],
+            [
+                'name' => 'Jon Lesaka',
+                'password' => bcrypt('hettich2025'),
+            ]
+        );
+
+        User::updateOrCreate(
+            ['email' => 'michel.saez.de.ojer@hettich.com'],
+            [
+                'name' => 'Miche Saez de Ojer',
+                'password' => bcrypt('hettich2025'),
+            ]
+        );
+
+        User::updateOrCreate(
+            ['email' => 'raul.lorenzo@hettich.com'],
+            [
+                'name' => 'Raul Lorenzo',
+                'password' => bcrypt('hettich2025'),
+            ]
+        );
+
+        User::updateOrCreate(
+            ['email' => 'alfonso.mitxelena@hettich.com'],
+            [
+                'name' => 'Alfonso Mitxelena',
+                'password' => bcrypt('hettich2025'),
+            ]
+        );
+
+        User::updateOrCreate(
+            ['email' => 'elena.aristegui@hettich.com'],
+            [
+                'name' => 'Elena Aristegui',
+                'password' => bcrypt('hettich2025'),
+            ]
+        );
+
+        User::updateOrCreate(
+            ['email' => 'javier.unanue@hettich.com'],
+            [
+                'name' => 'Javier Unanue',
+                'password' => bcrypt('hettich2025'),
+            ]
+        );
+
+        User::updateOrCreate(
+            ['email' => 'jesus.diaz@hettich.com'],
+            [
+                'name' => 'Jesus Diaz',
+                'password' => bcrypt('hettich2025'),
+            ]
+        );
+
+        User::updateOrCreate(
+            ['email' => 'felix.tena@hettich.com'],
+            [
+                'name' => 'Felix Tena',
+                'password' => bcrypt('hettich2025'),
             ]
         );
 
