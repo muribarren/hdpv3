292a0de83efd50898f14b44dd8bfbebdbecd9a28
Mikel Uribarren <mikel.uribarren@hettich.com>
2025-09-16 14:52:18 +0200

    otr

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -22,6 +22,24 @@
         >
             {{ __('VER HDP V2') }}
         </x-nav-link>
+
+        <x-nav-link 
+            href="https://apps01.es.hettich.com/vue_hdpv2" 
+            target="_blank" 
+            rel="noopener noreferrer"
+            class="bg-blue-600 text-white px-4 py-2 rounded hover:bg-blue-700 transition-colors duration-200"
+        >
+            {{ __('VER HDP V2') }}
+        </x-nav-link>
+
+        <x-nav-link 
+            href="apps01.es.hettich.com/vue_hdpv2" 
+            target="_blank" 
+            rel="noopener noreferrer"
+            class="bg-blue-600 text-white px-4 py-2 rounded hover:bg-blue-700 transition-colors duration-200"
+        >
+            {{ __('VER HDP V2') }}
+        </x-nav-link>
     </div>
 </div>
     </x-slot>
